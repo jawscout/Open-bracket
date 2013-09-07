d5a9037fd895274461ab93d778f0d8cc30b1a06f
Jake Wilkinson <jawscout@gmail.com>
2013-09-07 16:37:53 -0500

    Fixed errors

--- src/open/bracket/framework/test/TestRound.java
+++ src/open/bracket/framework/test/TestRound.java
@@ -1,7 +1,32 @@
 package open.bracket.framework.test;
 
 import open.bracket.framework.game.Round;
+import open.bracket.framework.game.player.Player;
 
 public class TestRound implements Round{
+
+	@Override
+	public void setPlayers(Player<? extends Player<?>>[] players) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Player<? extends Player<?>>[] getPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeRound() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isRoundFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
